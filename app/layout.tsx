70c24e5797455e873c423d6affe875d60327e7f3
reddy-lalith <lalithreddy7@gmail.com>
2025-06-18 02:17:55 -0400

    added new page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Rate My Mate',
+  description: 'Find honest reviews from past partners. Make more informed decisions about your dating life.',
 }
 
 export default function RootLayout({
